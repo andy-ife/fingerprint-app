63727ebf1ae5d576422e65392a42fb5e4d9ceaf8
WV SL <iomole3@gmail.com>
2026-06-08 22:41:45 +0100

    Remove comPort from biometric config

--- src/main/java/com/andyslab/biometric/service/model/BiometricConfig.java
+++ src/main/java/com/andyslab/biometric/service/model/BiometricConfig.java
@@ -36,7 +36,6 @@ public class BiometricConfig implements Serializable {
     private BiometricTemplateFormat templateFormat;
     private Integer deviceName;
     private Integer ajpPort;
-    private Integer comPort;
 
     // ***** PROPERTY ACCESS *****
 
@@ -121,13 +120,5 @@ public class BiometricConfig implements Serializable {
 
     public void setTemplateFormat(BiometricTemplateFormat templateFormat) {
         this.templateFormat = templateFormat;
-    }
-
-    public Integer getComPort() {
-        return comPort;
-    }
-
-    public void setComPort(Integer comPort) {
-        this.comPort = comPort;
     }
 }
